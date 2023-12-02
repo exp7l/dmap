e59b8e26cb70e657b611ad014143ea7e95197674
exponential <107167107+exp7l@users.noreply.github.com>
2023-12-01 20:13:46 -0500

    [test] add tests for locking and comments

--- test/Integration.t.sol
+++ test/Integration.t.sol
@@ -7,18 +7,37 @@ import {ZoneLike} from "../src/ZoneLike.sol";
 
 contract IntegrationTest is Test, BaseTest {
     function test_SetKey() public {
+        ///// definitions /////
         // :free:vitalik
         string memory namePlain = "vitalik";
         // salt does not matter because appraisal is 0
         free.assume(bytes32(uint256(426942)), namePlain);
         string memory keyPlain = "primary-wallet";
-        bytes memory expectedValue = abi.encode(address(0x696969));
+        bytes memory expectedValue = abi.encode("abc");
 
-        // IMPORTANT
+        ///// free.set 1 /////
         bytes32 mapId = _setKey(free, namePlain, keyPlain, expectedValue);
 
         bytes32 physicalKey = keccak256(abi.encode(mapId, _key(keyPlain)));
         require(keccak256(emap.get(physicalKey)) == keccak256(expectedValue));
+
+        bytes32 name = keccak256(abi.encode(namePlain));
+        bytes32 slot = keccak256(abi.encode(address(free),name));
+        (bytes32 meta, bytes32 data) = dmap.get(slot);
+        require(data == mapId);
+
+        ///// free.set 2 /////
+        bytes memory expectedValue2 = abi.encode("bcd");
+        _setKey(free, namePlain, keyPlain, expectedValue2);
+        require(keccak256(emap.get(physicalKey)) == keccak256(expectedValue2));
+
+        ///// free.set 3 (lock) /////
+        bytes32 LOCK = bytes32(uint256(1));
+        free.set(name, LOCK, mapId);
+
+        ///// revert on set after lock /////
+        vm.expectRevert(bytes("ERR_LOCKED"));
+        _setKey(free, namePlain, keyPlain, expectedValue2);
     }
 
     function _setKey(ZoneLike zone, string memory namePlain, string memory keyPlain, bytes memory value)
