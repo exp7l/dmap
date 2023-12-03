48db2f2ac6c35ddf9eda3b3a9fdc7ef14999f37e
exponential <107167107+exp7l@users.noreply.github.com>
2023-12-03 11:34:50 -0500

    [emap] comments

--- src/EmapLike.sol
+++ src/EmapLike.sol
@@ -2,20 +2,11 @@
 pragma solidity 0.8.13;
 
 struct Key {
-    // type tag convention:
-    // 1. bool
-    // 2. uint256
-    // 3. int256
-    // 4. address
-    // 5. bytes32
-    // 6. bytes
-    // 7. string
     uint8 typ;
     bytes24 key;
     bytes32 mapId;
 }
 
-// Elastic Map
 interface EmapLike {
     event Set(
         address indexed zone, bytes32 indexed mapId, bytes24 indexed key, uint8 indexed typ, bytes value
